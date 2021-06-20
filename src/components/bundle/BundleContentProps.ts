1ebdb979bf5b0911b3719f348b75a34905e0f0e1
JustinLohKingWei <justinlohkingwei@gmail.com>
2021-06-20 19:56:32 -0400

    Added some dummy content props (unused)

--- src/components/bundle/BundleContentProps.ts
+++ src/components/bundle/BundleContentProps.ts
@@ -6,4 +6,48 @@ type BundleContentProps = {
   open: () => void;
 };
 
+type ItemProps = {
+  itemName: string;
+  itemPublisher: string;
+  itemPrice: number;
+  itemImage: string;
+};
+
+type BundleProps = {
+  bundleTitle: string;
+  bundlePrice: number;
+  bundleCollapsedImage: string;
+  bundleExpandedImage: string;
+  bundleItems: [ItemProps];
+};
+
+const itemTest = [
+  {
+    itemTitle: "Beyond Two Souls",
+    itemPublisher: "Quantic Dream LLC",
+    itemPrice: 29.99,
+    itemImage: "beyond.jpg",
+  },
+  {
+    itemTitle: "Heavy Rain",
+    itemPublisher: "Quantic Dream LLC",
+    itemPrice: 19.99,
+    itemImage: "rain.jpg",
+  },
+  {
+    itemTitle: "Detroit Become Human",
+    itemPublisher: "Quantic Dream LLC",
+    itemPrice: 79.99,
+    itemImage: "image_placeholder3.jpg",
+  },
+];
+
+const bundleTest = {
+  bundleName: "Quantic Dream Deluxe Bundle",
+  bundlePrice: 180.99,
+  bundleCollapsedImage: "image_placeholder1.jpg",
+  bundleExpandedImage: "image_placeholder2.jpg",
+  bundleItems: itemTest
+};
+
 export default BundleContentProps;
